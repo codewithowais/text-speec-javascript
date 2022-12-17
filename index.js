18ea2a9ea4ee6af3472f8940c72b929aa5a123d2
Muhammad Owais Ahmed <owaisraza2051@gmail.com>
2022-12-17 18:21:54 +0500

    text speech javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+const textInputField = document.querySelector("#text-input");
+const myForm = document.querySelector("#form");
+const utterThis = new SpeechSynthesisUtterance();
+const synth = window.speechSynthesis;
+let ourText = "";
+
+const checkBrowserCompatibility = () => {
+  "speechSynthesis" in window
+    ? console.log("Web Speech API supported!")
+    : console.log("Web Speech API not supported :-(");
+};
+
+checkBrowserCompatibility();
+
+myForm.onsubmit = (event) => {
+  event.preventDefault();
+  ourText = textInputField.value;
+  utterThis.text = ourText;
+  synth.speak(utterThis);
+  alert("Hello World");
+  textInputField.value = "";
+};
